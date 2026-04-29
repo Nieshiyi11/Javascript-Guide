450536309d09833dcaed5e5fce0a0d9979f12344
Nieshiyi11 <elevennie11@gmail.com>
2026-04-29 22:56:57 +0800

    修改了一下项目结构，加入了练习题。

--- JS_Test/Drop-down Menu.js
+++ JS_Test/Drop-down Menu.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("toggleBtn");
 const menu = document.getElementById("menu");
-btn.addEventListener("click", () => {
+btn.addEventListener("click", function(){
   menu.classList.toggle("open");
 });
